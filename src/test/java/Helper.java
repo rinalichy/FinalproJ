2ae918d635434e7d2a55cad5b6ab9f80cbb548a9
rina_l <marinal022@outlook.com>
2022-09-24 01:56:42 +0300

    ver1

---
+++ src/test/java/Helper.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class Helper {
+      public static void setupdriver (){
+        System.setProperty("webdriver.chrome.driver","res/chromedriver");
+     }
+
+    public static final String WIKIMAINURL = "https://en.wikipedia.org/wiki/Main_Page";
+    public static final String WIKIHELICOPTER = "https://en.wikipedia.org/wiki/Helicopter";
+    public static final String WIKIAIRPLAN = "https://en.wikipedia.org/wiki/Airplane";
+
+    public static final String SEARCHFIELDID = "searchInput";
+    public static final String SERCHBUTTONID = "searchButton";
+
+
+
+
+
+
+
+}
