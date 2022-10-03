56e230266b068487e395287c41d993033ef1ed0f
rina_l <marinal022@outlook.com>
2022-10-03 19:31:47 +0300

    ver 5 (final)

--- src/test/java/Helper.java
+++ src/test/java/Helper.java
@@ -24,7 +24,7 @@ public class Helper {
     public static final String CHECKBOXURl = "https://demoqa.com/checkbox";
     public static final String OPENOPTIONSXpath = "//*[@id=\"tree-node\"]/ol/li/span/button";
     public static final String MAINCHECKBOXpath = "//*[@id=\"tree-node\"]/ol/li/span/label/span[1]";
-    public static final String CHECKBOXDOCUMENTS = "//*[@id=\"tree-node\"]/ol/li/ol/li[2]/span/label/span[1]/svg";
+    public static final String CHECKBOXDOCUMENTS = "//*[@id=\"tree-node\"]/ol/li/ol/li[2]/span/label/span[1]";
     public static final String CLICKBUTTONURL= "https://demoqa.com/buttons";
     public static final String DOUBLEId = "doubleClickBtn";
     public static final String RIGHTCLICKId = "doubleClickBtn";
--- src/test/java/MainProject.java
+++ src/test/java/MainProject.java
@@ -16,7 +16,7 @@ import static java.lang.Thread.sleep;
 
 public class MainProject {
     @Test
-    void setUPtest(){
+    void setupTest(){
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.close();
@@ -24,34 +24,25 @@ public class MainProject {
 
 
     @Test
-    public static void navigateTEST() throws InterruptedException {
+    public static void navigateTest() throws InterruptedException {
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.get(Helper.WIKIMAINURL);
-        sleep(3000);
         driver.navigate().to(Helper.WIKIHELICOPTER);
-        sleep(3000);
         driver.navigate().to(Helper.WIKIAIRPLAN);
-        sleep(3000);
         driver.navigate().back();
-        sleep(3000);
         driver.navigate().forward();
-        sleep(3000);
         driver.navigate().refresh();
-        sleep(3000);
         driver.close();
     }
     @Test
-    public static void screensizeTEST() throws InterruptedException {
+    public static void screenSizeTest() throws InterruptedException {
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.get(Helper.WIKIMAINURL);
         driver.manage().window().maximize();
-        sleep(3000);
         driver.manage().window().fullscreen();
-        sleep(3000);
         driver.manage().window().minimize();
-        sleep(3000);
         driver.close();
     }
 
@@ -61,18 +52,14 @@ public class MainProject {
         WebDriver driver = new ChromeDriver();
         driver.get(Helper.WIKIMAINURL);
         String windowOne = driver.getWindowHandle();
-        Thread.sleep(2000);
         driver.switchTo().newWindow(WindowType.WINDOW);
-        sleep(2000);
         driver.get(Helper.WIKIAIRPLAN);
         String windowTwo = driver.getWindowHandle();
-        sleep(2000);
         driver.switchTo().window(windowOne);
         driver.close();
 
         driver.switchTo().window(windowTwo);
         driver.switchTo().newWindow(WindowType.TAB);
-        sleep(2000);
         driver.get(Helper.WIKIHELICOPTER);
         String tabTwo = driver.getWindowHandle();
         driver.close();
@@ -82,7 +69,7 @@ public class MainProject {
 
 
     @Test
-    public static void copypasteTest() throws IOException {
+    public static void copyPasteTest() throws IOException {
         File wikitext = new File(Helper.DPATH + "wikiFile" + Helper.TXT);
         try {
             if (wikitext.createNewFile()){
@@ -114,7 +101,7 @@ public class MainProject {
         driver.close();
     }
     @Test
-    public static void actionstest () throws InterruptedException, IOException {
+    public static void actionsTest () throws InterruptedException, IOException {
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.get(SiteData.ASOSURL);
@@ -151,37 +138,11 @@ public class MainProject {
         File fileh = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         File fileh1 = new File(Helper.DPATH +"VOUCHERgift" + Helper.JPG);
         FileUtils.copyFile(fileh,fileh1);
-
         driver.close();
-
-       //
-        //scroll till the end
-        // click button
-        // equal between two URL's
-        // scroll till step1
-        // click on pink color
-        // scroll to the 2nd step
-        // click on amount:250 sh
-        // scroll till 3rd step
-        // click on email field
-        // insert email field
-        // click on name field
-        // insert the RECIPIENT'S NAME
-        // scroll till "from"
-        // click on your name field
-        //insert your name
-        // click on drop down menu
-        // choose 240 sh
-        // click on pay now button
-
-       //
-      //  wait.until(ExpectedConditions.presenceOfElementLocated(By.id(SiteData.))).click();
-      //  wait.until(ExpectedConditions.presenceOfElementLocated(By.id(SiteData.))).click();
-
     }
 
     @Test
-    public static void checkboxes() throws IOException, InterruptedException {
+    public static void checkBoxesTest() throws IOException, InterruptedException {
     Helper.setupdriver();
     WebDriver driver =  new ChromeDriver();
     driver.get(Helper.CHECKBOXURl);
@@ -189,34 +150,38 @@ public class MainProject {
     WebElement check1 =
             wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Helper.OPENOPTIONSXpath)));
     check1.click();
+    WebElement littleCheck =
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Helper.CHECKBOXDOCUMENTS)));
+    littleCheck.click();
+    Thread.sleep(3000);
+    File file01 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    File file02 = new File(Helper.DPATH +"Checkbox" + Helper.JPG);
+    FileUtils.copyFile(file01,file02);
+    driver.close();
+
     }
 
-      //  File file01 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-      //  File file02 = new File(Helper.DPATH +"Checkbox" + Helper.JPG);
-     //   FileUtils.copyFile(file01,file02);
-      //  Thread.sleep(3000);
-      //  driver.close();
 
 
 
     @Test
-    public static void clicks () throws InterruptedException, IOException {
+    public static void clicksTest () throws InterruptedException, IOException {
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.get(Helper.CLICKBUTTONURL);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         Actions actions = new Actions(driver);
-        WebElement optonsRightclick =
+        WebElement optonsRightClick =
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.RIGHTCLICKId)));
-        actions.contextClick(optonsRightclick).perform();
+        actions.contextClick(optonsRightClick).perform();
         Thread.sleep(3000);
         File file01 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        File file02 = new File(Helper.DPATH +"OPTINCLICK" + Helper.JPG);
+        File file02 = new File(Helper.DPATH +"OPTIONCLICK" + Helper.JPG);
         FileUtils.copyFile(file01,file02);
         Thread.sleep(3000);
-        WebElement doulbelclick =
+        WebElement doulbelClick =
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.DOUBLEId)));
-        actions.doubleClick(doulbelclick).perform();
+        actions.doubleClick(doulbelClick).perform();
         File file1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         File file2 = new File(Helper.DPATH +"doubleclick" + Helper.JPG);
         sleep(3000);
@@ -224,24 +189,24 @@ public class MainProject {
     }
 
     @Test
-    public static void alert () throws InterruptedException {
+    public static void alertTest () throws InterruptedException {
         Helper.setupdriver();
         WebDriver driver = new ChromeDriver();
         driver.get(Helper.ALERTURL);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement alertbutton =
+        WebElement alertButton =
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.ALERTBUTTONid)));
-        alertbutton.click();
+        alertButton.click();
         Thread.sleep(2000);
         driver.switchTo().alert().accept();
-        WebElement secondalert =
+        WebElement secondAlert =
                 wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.ALERTBUTTONaccept_dissnisID)));
-        secondalert.click();
+        secondAlert.click();
         driver.switchTo().alert().dismiss();
         sleep(2000);
+        File file1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File file2 = new File(Helper.DPATH +"alertmassege" + Helper.JPG);
         driver.close();
-
-
     }
 
 
